5483de46549de601ec46a029cc7da10fe8070468
mojahidjouel <mojahidjouel95@gmail.com>
2023-12-27 09:14:33 +0600

    BUG FIX

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TypewriterComponent } from './typewriter/typewriter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypewriterComponent
   ],
   imports: [
     BrowserModule
